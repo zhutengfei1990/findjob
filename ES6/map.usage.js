a52029c3c605cb19ab3585d7b93ef4169a26d3bd
zhutengfei1990 <1041885117@qq.com>
2018-11-25 22:37:25 +0800

    documentFragment

--- ES6/map.usage.js
+++ ES6/map.usage.js
@@ -7,4 +7,10 @@ const map = new Map();
 
 items.forEach(([key, value]) => map.set(key, value));
 
-console.log(map);
+// 数组去重
+console.log([... new Set([1, 2, 2, 4])]);
+
+let [a, b, c] = [1, 2, 3];
+console.log(a, b, c);
+let {aa, bb} = {'aa': 1, 'bb': 2}
+console.log(aa, bb);
