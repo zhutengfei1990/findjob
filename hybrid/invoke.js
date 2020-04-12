c24b7f47dd1099d5dfdfeaf493fb1098b4351f11
zhutengfei1990 <1041885117@qq.com>
2020-04-12 12:35:32 +0800

    schema协议

--- hybrid/invoke.js
+++ hybrid/invoke.js
@@ -18,6 +18,21 @@
       callbackName = action + Date.now()
       window[callbackName] = callback
     }
+
+    schema +='callback=' + callbackName;
+
+    var iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    // 调用schema协议
+    iframe.src = schema;
+    // iframe.src = 'weixin://dl/scan?k1=v1&k2=v2&callback=_invoke_scan_callback_'
+    var body = document.body
+    body.append(iframe)
+    setTimeout(function() {
+      body.removeChild(iframe)
+      iframe = null
+    })
+    
   }
 
   window.invoke = {
